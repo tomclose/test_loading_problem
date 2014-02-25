2845ca980694a1148c064d5d06d02cd19048c409
Tom Close <tom.close@cantab.net>
2014-02-25 23:36:36 +0000

    Recreate test problem

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,10 @@
 export default Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+
+      setTimeout(function(){
+        resolve(['red', 'yellow', 'blue']);
+      }, 100);
+    });
   }
 });
